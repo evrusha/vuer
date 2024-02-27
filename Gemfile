d41f9e429c05d6b74536afb95dd3aa226df8fcf5
evrusha <uzaharau@gmail.com>
2024-02-27 19:54:35 +0300

    switch to postgres

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source "https://rubygems.org"
 ruby "3.2.2"
 
 gem "rails", "~> 7.1.3", ">= 7.1.3.2"
-gem "mysql2", "~> 0.5"
 gem 'pg'
 gem "puma", ">= 5.0"
 gem "jbuilder"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_02_27_094946) do
-  create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.float "price"
